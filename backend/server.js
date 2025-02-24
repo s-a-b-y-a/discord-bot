41c8e40ad46df62ae706ecceb380d0222e1727fb
Sabyasachi Biswal <sabyas2004@gmail.com>
2025-02-25 00:30:51 +0530

    some port fixes

--- backend/server.js
+++ backend/server.js
@@ -14,3 +14,9 @@ app.use('/', URLroutes)
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 .then(() => console.log('coonected to the Database'))
 .catch((err) => console.error('Database connection error: ', err))
+
+const PORT = process.env.PORT || 3000; 
+
+app.listen(PORT, "0.0.0.0", () => {  
+  console.log(`Server is running on port ${PORT}`);
+});

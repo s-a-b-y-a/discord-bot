a8e63ffe4e26a4db6610be944ac885703a1a671d
Sabyasachi Biswal <sabyas2004@gmail.com>
2025-02-25 11:51:07 +0530

    bot fixes

--- bot/index.js
+++ bot/index.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 // Keep-alive route
 app.get("/", (req, res) => res.send("Bot is running!"));
 
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+app.listen(PORT, '0.0.0.0', () => console.log(`Server is running on port: ${PORT}`));
 
 const client = new Client({
   intents: [
